18c96ad4156d791a00238b165152a198c80c5565
Manny Verma <mannyverma11@gmail.com>
2024-06-12 15:12:32 -0600

    updating js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const shareButton = document.querySelector('.shareButton');
+// const shareButton = document.querySelector('.shareButton');
 
-document.querySelector('.shareButton').addEventListener('click', function () {
-	this.classList.toggle('active');
-	shareButton.classList.toggle('active');
-});
-
-// const shareIcon = document.getElementById('.shareIcon');
-// const shareButton = (document.getElementById = '.shareButton');
-
-// shareIcon.addEventListener('click', () => {
-// 	shareButton.classList.toggle('visible');
-// 	if (shareButton.classList.contains('visible')) {
-// 	}
+// document.querySelector('.shareButton').addEventListener('click', function () {
+// 	this.classList.toggle('active');
+// 	shareButton.classList.toggle('active');
 // });
+
+const shareIcon = document.getElementById('.shareIcon');
+const shareButton = (document.getElementById = '.shareButton');
+
+shareIcon.addEventListener('click', () => {
+	shareButton.classList.toggle('visible');
+	if (shareButton.classList.contains('visible')) {
+	}
+});
 
 // const closeBtn = document.getElementById('.buttonCloseOne');
 
